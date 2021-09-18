8912c3b0be89887bd735e43e17f4b544282c242d
Mucotoo <77805193+Mucotoo@users.noreply.github.com>
2021-09-18 17:08:19 +0300

    Create enckey.lua

---
+++ enckey.lua
@@ -0,0 +1,10 @@
+if Key == 1 then 
+Sha= math.random(27291,238218)
+print(sha1("1+"..Sha..""))
+end
+if Key == 2 then 
+for i = 1,10 do 
+Sha= math.random(27291,238218)
+print(sha1("1+"..Sha..""))
+end
+end
